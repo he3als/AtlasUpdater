579276d8e5dbe038708c121a305320cff86e5cd3
he3als <65787561+he3als@users.noreply.github.com>
2022-03-12 17:03:19 +0000

    Version 1.0
    everything should be good now :)

--- AtlasUpdaterScript.bat
+++ AtlasUpdaterScript.bat
@@ -1,10 +1,10 @@
 @echo off
-color 03
+color 0b
 :: Here you can disable/enable automatic updates, true/false
 set automaticupdatecheck=true
-set version=Version: 1.0
-set menucls=true
-title Atlas Update Script - made by he3als - %version%
+:: You shouldn't need to change anything from here
+set version=1.0
+title Atlas Update Script - Made by he3als - Version: %version%
 fltmc >nul 2>&1 || (
     echo Administrator privileges are required.
     PowerShell -NoProfile Start -Verb RunAs '%0' 2> nul || (
@@ -15,68 +15,79 @@ fltmc >nul 2>&1 || (
 )
 
 :checkforupdatesa
-if %automaticupdatecheck%==false goto menu
-Powershell.exe -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/he3als/AtlasUpdater/main/VERSION' -OutFile '%USERPROFILE%\Downloads\VERSION'"
+if %automaticupdatecheck%==false set updated=disabled & goto versiondisplay
+del /f %USERPROFILE%\Downloads\VERSIONATLASUPDATER >nul 2>&1
+Powershell.exe -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/he3als/AtlasUpdater/main/VERSIONATLASUPDATER' -OutFile %USERPROFILE%\Downloads\VERSIONATLASUPDATER"
 if %ERRORLEVEL%==0 (
-echo Downloaded VERSION file to check if there's updates...
+echo Downloaded VERSIONATLASUPDATER file to check if there's updates...
+echo.
 goto checkforupdatesb
 ) ELSE (
+cls
 set updated=failed
 echo Failed checking for updates!
 echo You could be offline or the repository may be inaccessable.
 echo You can disable update checking by editing the batch script.
 echo You will now be taken to the menu.
 pause
-goto menu
+goto versiondisplay
 )
 
 :checkforupdatesb
-findstr /c:"%version%" "%USERPROFILE%\Downloads\VERSION" && (
-  set updated=true
-  del /f %USERPROFILE%\Downloads\VERSION
-  goto versiondisplay
-) || (
-  for /f %%i in ('findstr /c:"Version" "%USERPROFILE%\Downloads\VERSION"') do set newversion=%%i
-  set updated=false
-  del /f %USERPROFILE%\Downloads\VERSION
-  echo Tip: You can disable automatic update checking by easily editing the batch script.
-  echo An update was found!
-  echo CHOICE /N /C:YN /M "Would you like to download the latest version of the script and output it to your Downloads folder? [Y/N]"
-  IF %ERRORLEVEL%==1 goto update
-  IF %ERRORLEVEL%==2 goto menu
+findstr /c:"%version%" "%USERPROFILE%\Downloads\VERSIONATLASUPDATER"
+if %ERRORLEVEL%==0 (
+set updated=true
+del /f %USERPROFILE%\Downloads\VERSIONATLASUPDATER
+goto versiondisplay
+) ELSE (
+goto updateavaliable
 )
 
+:updateavaliable
+for /f %%i in ('Powershell.exe -NoProfile -NoLogo -Command "Get-Content %USERPROFILE%\Downloads\VERSIONATLASUPDATER -First 1"') do set newversionnumber=%%i
+set updated=false
+echo Tip: You can disable automatic update checking by easily editing the batch script.
+echo An update was found!
+CHOICE /N /C:YN /M "Would you like to download the latest version of the script and output it to your Downloads folder? [Y/N]"
+IF %ERRORLEVEL%==1 goto update
+IF %ERRORLEVEL%==2 goto versiondisplay
+
 :update
-Powershell.exe -NoProfile -Command "$regex = '[0-9]+.[0-9]+'; select-string -Path %USERPROFILE%\Downloads\VERSION -Pattern $regex -AllMatches | % { $_.Matches } | % { $_.Value } > $env:versionnumberexact"
-if %ERRORLEVEL%==0 (echo Set version number in PowerShell...
-) ELSE (echo Failed setting version number in PowerShell!&pause&exit)
-Powershell.exe -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/he3als/AtlasUpdater/main/AtlasUpdaterScript.bat' -OutFile '%USERPROFILE%\Downloads\AtlasUpdaterScript $env:versionnumberexact.bat'"
+cls
+set %ERRORLEVEL%==0
+for /f %%i in ('Powershell.exe -NoProfile -NoLogo -Command "Get-Content %USERPROFILE%\Downloads\VERSIONATLASUPDATER -First 1"') do set newversionnumber=%%i
+:: Error detection always fails here? Not sure why, the rest of the script seemingly works fine...
+:: if %ERRORLEVEL%==0 (echo Set version number in PowerShell...
+:: ) ELSE (echo Failed setting version number in PowerShell^!&pause&exit)
+Powershell.exe -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/he3als/AtlasUpdater/main/AtlasUpdaterScript.bat' -OutFile %USERPROFILE%\Downloads\AtlasUpdaterScript_$env:newversionnumber.bat"
 if %ERRORLEVEL%==0 (echo Downloaded new script...
 ) ELSE (echo Failed downloading the new script!&pause&exit)
+echo.
 echo Finished updating!
-echo CHOICE /N /C:12 /M "Would you like to exit or execute and exit the updated script? [1/2]"
-IF %ERRORLEVEL%==1 goto exit
-IF %ERRORLEVEL%==2 goto openupdatedscript
+echo What would you like to do?
+echo 1. Execute the new script and exit
+echo 2. Exit
+CHOICE /N /C:12 /M "Please input your answer here ->"
+IF %ERRORLEVEL%==1 goto openupdatedscript
+IF %ERRORLEVEL%==2 goto exit
 
 :openupdatedscript
-start "" "%USERPROFILE%\Downloads\AtlasUpdaterScript %versionnumberexact%.bat"
-if %ERRORLEVEL%==0 (echo Executed the new script!
-) ELSE (echo Failed executing the new script!&pause&exit)
-pause
+start "" %USERPROFILE%\Downloads\AtlasUpdaterScript_%newversionnumber%.bat
+color 0a
+echo Executed the new script!
 exit
 
 :versiondisplay
-color 03
 cls
-set menucls=false
+color 0b
 if %updated%==true (echo Up to date!) else (echo Outdated version!)
-if %updated%==false (echo New %newversion%)
+if %updated%==false (echo New Version: %newversionnumber%)
 if %updated%==failed (echo Checking for updates failed!)
+if %updated%==disabled (echo Checking for updates disabled!)
+echo Current Version: %version%
+echo.
 
 :menu
-color 03
-if %menucls%==true (cls)
-echo Current %version%
 echo Warning: This script can do un-wanted changes to your computer, use with caution and review the script first!
 echo This script is for https://github.com/Atlas-OS/Atlas - a Windows mod for mainly gaming.
 echo What would you like to do?
@@ -88,8 +99,8 @@ echo 5. Exit
 CHOICE /N /C:12345 /M "Please input your answer here ->"
 IF %ERRORLEVEL%==1 goto postinstall
 IF %ERRORLEVEL%==2 goto updatefolders
-IF %ERRORLEVEL%==3 start "" "https://github.com/Atlas-OS/Atlas" & goto menu
-IF %ERRORLEVEL%==4 start "" "https://github.com/he3als/AtlasUpdater" & goto menu
+IF %ERRORLEVEL%==3 start "" "https://github.com/Atlas-OS/Atlas" & cls & goto versiondisplay
+IF %ERRORLEVEL%==4 start "" "https://github.com/he3als/AtlasUpdater" & cls & goto versiondisplay
 IF %ERRORLEVEL%==5 exit
 
 :postinstall
@@ -97,25 +108,28 @@ cls
 echo You should update Atlas^' folders before doing this, doing this is dangerous and can cause issues!
 CHOICE /N /C:YN /M "Continue? [Y/N]"
 IF %ERRORLEVEL%==1 goto postinstallconfirma
-IF %ERRORLEVEL%==2 goto menu
+IF %ERRORLEVEL%==2 goto versiondisplay
 
 :postinstallconfirma
 cls
 color 4F
-echo Are you 100% sure? This will run the post install script that first runs when you install Atlas and this is potentially dangerous to do.
+echo Are you 100^% sure? 
+echo This will run the post install script that first runs when you install Atlas and this is potentially dangerous to do.
 timeout /T 10 /NOBREAK
 CHOICE /N /C:YN /M "Continue? [Y/N]"
-IF %ERRORLEVEL%==1 start "C:\Windows\AtlasModules\nsudo.exe -U:T -P:E -Wait C:\Windows\AtlasModules\atlas-config.bat /start"
-IF %ERRORLEVEL%==2 goto menu
+IF %ERRORLEVEL%==1 start "" "C:\Windows\AtlasModules\nsudo.exe -U:T -P:E -Wait C:\Windows\AtlasModules\atlas-config.bat /start"
+IF %ERRORLEVEL%==2 goto versiondisplay
 
 :updatefolders
 cls
 echo Warning: Your current Atlas folder on your desktop and AtlasModules will be deleted permenantly!
 CHOICE /N /C:YN /M "Continue? [Y/N]"
 IF %ERRORLEVEL%==1 goto updatefoldersconfirmed
-IF %ERRORLEVEL%==2 goto menu
+IF %ERRORLEVEL%==2 goto versiondisplay
 
 :updatefoldersconfirmed
+rd /s /q %USERPROFILE%\Downloads\tempatlas >nul 2>&1
+del /f %USERPROFILE%\Downloads\tempatlas.zip >nul 2>&1
 Powershell.exe -NoProfile -Command "Invoke-WebRequest -Uri 'https://github.com/Atlas-OS/Atlas/archive/refs/heads/main.zip' -OutFile '%USERPROFILE%\Downloads\tempatlas.zip'"
 if %ERRORLEVEL%==0 (echo Downloaded Atlas zip successfully.
 ) ELSE (echo Failed downloading Atlas zip.&pause&exit)
@@ -125,19 +139,27 @@ if %ERRORLEVEL%==0 (echo Expanded the Atlas zip successfully.
 goto delfolders
 
 :delfolders
-rd /s %USERPROFILE%\Desktop\Atlas
+rd /s /q %USERPROFILE%\Desktop\Atlas
 if %ERRORLEVEL%==0 (echo Deleted Atlas desktop folder
-) ELSE (echo Failed deleting Atlas desktop folder.&pause&exit)
-rd /s C:\Windows\AtlasModules
-if %ERRORLEVEL%==0 (echo Deleted AtlasModules folder
-) ELSE (echo Failed deleting AtlasModules folder.&pause&exit)
+) ELSE (
+echo Failed deleting Atlas desktop folder.
+CHOICE /N /C:YN /M "Continue anyways? [Y/N]"
+IF %ERRORLEVEL%==2 exit
+)
+rd /s /q C:\Windows\AtlasModules
+if %ERRORLEVEL%==0  (echo Deleted AtlasModules folder
+) ELSE (
+echo Failed deleting AtlasModules folder.
+CHOICE /N /C:YN /M "Continue anyways? [Y/N]"
+IF %ERRORLEVEL%==2 exit
+)
 goto copyfolders
 
 :copyfolders
-xcopy "%USERPROFILE%\Downloads\tempatlas\Atlas-main\src\AtlasModules" "C:\Windows\AtlasModules" /e /i /h
+xcopy "%USERPROFILE%\Downloads\tempatlas\Atlas-main\src\AtlasModules" "C:\Windows\AtlasModules" /e /i /h /q > nul
 if %ERRORLEVEL%==0 (echo Copied AtlasModules folder
 ) ELSE (echo Failed copying the AtlasModules folder.&pause&exit)
-xcopy "%USERPROFILE%\Downloads\tempatlas\Atlas-main\src\Desktop\Atlas" "%USERPROFILE%\Desktop\Atlas" /e /i /h
+xcopy "%USERPROFILE%\Downloads\tempatlas\Atlas-main\src\Desktop\Atlas" "%USERPROFILE%\Desktop\Atlas" /e /i /h /q > nul
 if %ERRORLEVEL%==0 (echo Copied Atlas desktop folder
 ) ELSE (echo Failed copying the Atlas desktop folder.&pause&exit)
 goto deletetemp
@@ -146,6 +168,7 @@ goto deletetemp
 rd /s /q "%USERPROFILE%\Downloads\tempatlas"
 del /f "%USERPROFILE%\Downloads\tempatlas.zip"
 color 0a
+echo.
 echo Completed without errors!
 pause
-goto menu
+goto versiondisplay
